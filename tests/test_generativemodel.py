aef15e49da15a6b10018461ac786524e4714472c
SAKAI Yoshiki <sakai@sakaiyoshiki.dev>
2024-09-18 23:18:39 +0900

    BinaryEnergyBasedModelの簡易的な学習に成功

--- tests/test_generativemodel.py
+++ tests/test_generativemodel.py
@@ -32,30 +32,17 @@ def test_BinaryEnegeyBasedModel_確率確認():
 
     # 検証
     assert probs.sum() == pytest.approx(1.0)
-    expected = np.array([[9.203804e-01, 3.761384e-03, 7.554943e-02, 3.087532e-04]]).T
+    expected = np.array([9.203804e-01, 3.761384e-03, 7.554943e-02, 3.087532e-04])
     np.testing.assert_allclose(probs, expected, atol=1e-5)
 
 
 def test_BinaryEnegeyBasedModel_学習():
-    x = np.array(
-        [
-            [0, 0],
-            [0, 0],
-            [0, 0],
-            [0, 0],
-            [0, 0],
-            [1, 0],
-            [1, 1],
-            [1, 1],
-            [1, 1],
-            [1, 1],
-        ]
-    )
+    x = np.array([[0, 0]] * 500 + [[1, 0]] * 100 + [[1, 1]] * 400)
     enegey_func = MyNeuralNet(
         layers=[
-            Dense.init(d=2, M=2, rand=True),
+            Dense.init(d=2, M=4, seed=1234),
             ReLU(),
-            Dense.init(d=2, M=1, rand=True),
+            Dense.init(d=4, M=1, seed=1234),
         ],
         d_input=2,
         d_output=1,
@@ -65,11 +52,12 @@ def test_BinaryEnegeyBasedModel_学習():
     trained_ebm: MyBinaryEnergyBasedModel = train_generative_mgd(
         init_ebm=init_ebm,
         X=x,
-        n_epochs=1000,
-        batch_size=20,
+        n_epochs=10000,
+        batch_size=100,
         learning_rate=0.01,
         tolerance=1e-7,
     )
 
-    assert trained_ebm.prob(x=np.array([[0, 0]])) == pytest.approx(0.5)
-    assert trained_ebm.prob(x=np.array([[1, 1]])) == pytest.approx(0.4)
+    assert trained_ebm.prob(x=np.array([[0, 0]])) == pytest.approx(0.5, abs=1e-2)
+    assert trained_ebm.prob(x=np.array([[1, 0]])) == pytest.approx(0.1, abs=1e-2)
+    assert trained_ebm.prob(x=np.array([[1, 1]])) == pytest.approx(0.4, abs=1e-2)
